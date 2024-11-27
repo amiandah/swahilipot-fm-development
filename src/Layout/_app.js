7dceee31b9a160375da07654ffa56c5d71263927
ian-veteran <iankaranimutuma@gmail.com>
2024-11-27 12:17:16 +0300

    Enable audio playback while navigating diff pages

--- src/Layout/_app.js
+++ src/Layout/_app.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 import "@/styles/globals.css";
-import Layout from './Layout'; 
+import Layout from "./Layout";
+import { AudioContext, AudioProvider } from "@/context/AudioContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AudioProvider>
+      <PersistentAudioPlayer />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AudioProvider>
   );
 }
+
+const PersistentAudioPlayer = () => {
+  const { audioRef } = React.useContext(AudioContext);
+
+  return (
+    <audio ref={audioRef} style={{ display: 'none' }}>
+      <source src="https://swahilipotfm.out.airtime.pro/swahilipotfm_a?_ga=2.140975346.1118176404.1720613685-1678527295.1702105127" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+  );
+};
 
 export default MyApp;
--- src/context/AudioContext.js
+++ src/context/AudioContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useRef } from 'react';
+import React, { createContext, useState, useRef, useEffect } from "react";
 
 export const AudioContext = createContext();
 
@@ -6,20 +6,39 @@ export const AudioProvider = ({ children }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
 
-  const togglePlay = () => {
-    if (audioRef.current) {
-      if (isPlaying) {
-        audioRef.current.pause();
-      } else {
-        audioRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
+  useEffect(() => {
+    if (!audioRef.current) {
+      audioRef.current = new Audio(
+        "https://swahilipotfm.out.airtime.pro/swahilipotfm_a?_ga=2.140975346.1118176404.1720613685-1678527295.1702105127"
+      );
+      audioRef.current.loop = true;
     }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current = null;
+      }
+    };
+  }, []);
+
+  
+  const togglePlay = () => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
   };
 
   return (
-    <AudioContext.Provider value={{ isPlaying, setIsPlaying, togglePlay, audioRef }}>
+    <AudioContext.Provider
+      value={{ isPlaying, setIsPlaying, togglePlay, audioRef }}
+    >
       {children}
     </AudioContext.Provider>
   );
-}; 
+};
